17d91a93d22bd85e23621b9cb6514e0609d81526
wiparat131 <Tuktikt37@gmail.com>
2018-02-24 21:52:18 +0700

    Controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,7 @@
 package sample;
 
 import admin.adminController;
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXPasswordField;
-import com.jfoenix.controls.JFXTextField;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
